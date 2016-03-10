6bbbec4907d5a16dbf8c28a54ce7c2956ae95adf
D'Arcey Taylor <darceytaylor05@gmail.com>
2016-03-10 15:42:16 -0500

    initial commit

---
+++ server/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose = require('mongoose');
+//bcrypt = require(bcrypt);
+
+// DEFINE THE OBJECT SCHEMA
+var userSchema = new mongoose.Schema ({
+   created: {
+       type: Date,
+       default:Date.now
+   },
+   
+   username: {
+       type: String,
+       default: '',
+       trim: true,
+       required: 'Username is required',
+       index: {unique: true}
+   },
+   
+   password: {
+       type: String,
+       default: '',
+       trim: true,
+       required: 'Password is required'
+   },
+   
+   email: {
+       type: String,
+       default: '',
+       trim: true,
+       required: 'E-Mail is required'
+   }
+});
+
+// MAKE THIS PUBLIC SO THE CONTROLLER CAN SEE IT
+export var User = mongoose.model('User', userSchema);
